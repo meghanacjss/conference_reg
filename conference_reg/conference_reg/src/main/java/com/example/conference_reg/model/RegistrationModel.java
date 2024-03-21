6d82c0f998ec2865178cbcc056a244ea6b123d52
meghanacjss <159750961+meghanacjss@users.noreply.github.com>
2024-03-21 10:44:45 +0530

    Update RegistrationModel.java

--- conference_reg/conference_reg/src/main/java/com/example/conference_reg/model/RegistrationModel.java
+++ conference_reg/conference_reg/src/main/java/com/example/conference_reg/model/RegistrationModel.java
@@ -13,10 +13,8 @@ import java.util.Date;
 public class RegistrationModel {
 
     private int rid;
-    //  @Future(message="enter date must be in future")
+
     private String rdate;
-    //  @Min(value=1,message="amount must be greater than 1")
-  //  private long ramount;
 
     private Attendee attendee;
     private Event event;
