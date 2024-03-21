9357aaa599c91aa8e11a298e851c6e1bb6603516
meghanacjss <159750961+meghanacjss@users.noreply.github.com>
2024-03-21 10:42:24 +0530

    Update Registration.java

--- conference_reg/conference_reg/src/main/java/com/example/conference_reg/entity/Registration.java
+++ conference_reg/conference_reg/src/main/java/com/example/conference_reg/entity/Registration.java
@@ -15,19 +15,15 @@ public class Registration {
     @GeneratedValue
     private int rid;
 
-    // @JsonIgnore
     @ManyToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "eventId")
     private Event event;
 
-    // @JsonIgnore
     @ManyToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "attendeeId")
     private Attendee attendee;
     @DateTypeValidation
- //  @Future(message="enter date must be in future")
     private String rdate;
-//    @Min(value=1,message="amount must be greater than 1")
-//    private long ramount;
+
 
 }
