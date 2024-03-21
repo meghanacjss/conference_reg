5e453246c8522bae3bf3b4317eb4b35d72ed9b2e
meghanacjss <159750961+meghanacjss@users.noreply.github.com>
2024-03-21 10:43:55 +0530

    Update OwnerModel.java

--- conference_reg/conference_reg/src/main/java/com/example/conference_reg/model/OwnerModel.java
+++ conference_reg/conference_reg/src/main/java/com/example/conference_reg/model/OwnerModel.java
@@ -11,18 +11,14 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OwnerModel {
 
-    //    @NotBlank(message = "name must be fill")
-//    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-    //   @NotBlank(message="role not be blank")
+ 
     private String role;
-    // @Id
-    // @NotBlank(message = "Username not be blank")
+
     private String username;
-    //    @NotBlank(message = "Password not be blank")
-//    @Size(min = 6,message = "Password must be at least 6 characters")
+ 
     private String password;
-    //  @Email(message= "email should be in proper format")
+
     private String email;
 
 }
