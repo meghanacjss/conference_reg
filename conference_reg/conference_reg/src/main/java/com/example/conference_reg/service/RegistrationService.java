f612d9127a0ed162dd15db611429f0989353db3f
meghanacjss <159750961+meghanacjss@users.noreply.github.com>
2024-03-27 14:25:29 +0530

    Update RegistrationService.java

--- conference_reg/conference_reg/src/main/java/com/example/conference_reg/service/RegistrationService.java
+++ conference_reg/conference_reg/src/main/java/com/example/conference_reg/service/RegistrationService.java
@@ -17,6 +17,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 @Service
 public class RegistrationService implements RegistrationInter {
@@ -32,59 +33,30 @@ private EventRepository eventRepository;
     private EntityToModel entityToModel;
     @Autowired
     private ModelToEntity modelToEntity;
-
-
-
     public RegistrationModel createRegistration(RegistrationModel registrationModel, Event event, Attendee attendee) {
         List<Registration> registrationsForEvent = registrationRepository.findByEvent(event);
         System.out.println(registrationModel.getRdate());
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate newRegistrationDate = LocalDate.parse(registrationModel.getRdate(),dateFormat);
-
         boolean timingAvailable = checkTimingAvailability(registrationsForEvent, newRegistrationDate.toString());
-
         if (timingAvailable) {
             throw new IllegalArgumentException("Registration is already booked.");
         }
-        Registration registration = modelToEntity.convertToEntity5(registrationModel);
+        Registration registration = modelToEntity.registrationModelToEntity(registrationModel);
         registration.setEvent(event);
         registration.setAttendee(attendee);
-        return entityToModel.convertToModel5(registrationRepository.save(registration));
+        return entityToModel.registrationEntityToModel(registrationRepository.save(registration));
     }
     private boolean checkTimingAvailability(List<Registration> registrations, String newRegistrationDate) {
-
         return registrations.stream().anyMatch(date -> date.getRdate().equals(newRegistrationDate));
-
-
     }
-
-public List<RegistrationModel> getAllRegistrations() {
+    public List<RegistrationModel> getAllRegistrations(){
     return registrationRepository.findAll().stream()
             .sorted(Comparator.comparing(Registration::getRdate))
-            .map(entityToModel::convertToModel5)
+            .map(entityToModel::registrationEntityToModel)
             .collect(Collectors.toList());
 }
-
-    public List<RegistrationModel>getRegistrationsByEvent(Event event) {
-        List<Registration> registrations = registrationRepository.findByEvent(event);
-        return registrations.stream()
-                .map(entityToModel::convertToModel5)
-                .collect(Collectors.toList());
-    }
-    public List<RegistrationModel> getRegistrationsByAttendee(Attendee attendee) {
-        List<Registration> registrations = registrationRepository.findByAttendee(attendee);
-        return registrations.stream()
-                .map(entityToModel::convertToModel5)
-                .collect(Collectors.toList());
-    }
-    public RegistrationModel getRegistrationById(int rid) {
-        Registration registration = registrationRepository.findById(rid).orElse(null);
-        if (registration != null) {
-            return entityToModel.convertToModel5(registration);
-        }
-        return null;
-    }
-     public Event getEventWithMostRegistrations() {
+    public Event getEventWithMostRegistrations() {
         List<Registration> registrations = registrationRepository.findAll();
         Map<Event,Integer> eventCounts = registrations.stream()
                 .collect(Collectors.groupingBy(Registration::getEvent, Collectors.summingInt(registration->1)));
@@ -94,7 +66,48 @@ public List<RegistrationModel> getAllRegistrations() {
                 .map(Map.Entry::getKey)
                 .orElse(null);
     }
-public RegistrationModel cancelRegistration(int rid) {
+
+    /*public Event getEventWithMostRegistrations() {
+        List<Registration> registrations = registrationRepository.findAll();
+        Map<Event,Long> eventCounts = registrations.stream()
+                .collect(Collectors.groupingBy(Registration::getEvent, Collectors.counting()));
+
+        return eventCounts.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }*/
+
+    /*public Attendee getAttendeeWithHighestRegistrationsByEvent(Event event) {
+    List<Registration> registrations = registrationRepository.findByEvent(event);
+
+    Map<Attendee, Integer> attendeeCounts = registrations.stream()
+            .collect(Collectors.groupingBy(Registration::getAttendee, Collectors.summingInt(registration -> 1)));
+    return attendeeCounts.entrySet().stream()
+            .max(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey)
+            .orElse(null);
+}*/
+    public List<RegistrationModel>getRegistrationsByEvent(Event event) {
+        List<Registration> registrations = registrationRepository.findByEvent(event);
+        return registrations.stream()
+                .map(entityToModel::registrationEntityToModel)
+                .collect(Collectors.toList());
+    }
+    public List<RegistrationModel> getRegistrationsByAttendee(Attendee attendee) {
+        List<Registration> registrations = registrationRepository.findByAttendee(attendee);
+        return registrations.stream()
+                .map(entityToModel::registrationEntityToModel)
+                .collect(Collectors.toList());
+    }
+    public RegistrationModel getRegistrationById(int rid) {
+        Registration registration = registrationRepository.findById(rid).orElse(null);
+        if (registration != null) {
+            return entityToModel.registrationEntityToModel(registration);
+        }
+        return null;
+    }
+    public RegistrationModel cancelRegistration(int rid) {
     Registration registrationEntity = registrationRepository.findById(rid).orElse(null);
     if (registrationEntity != null) {
         List<Payment> payments = paymentRepository.findByRegistration(registrationEntity);
@@ -103,10 +116,9 @@ public RegistrationModel cancelRegistration(int rid) {
             paymentRepository.save(payment);
         });
         registrationRepository.delete(registrationEntity);
-        return entityToModel.convertToModel5(registrationEntity);
+        return entityToModel.registrationEntityToModel(registrationEntity);
     } else {
         throw new IllegalArgumentException("Registration not found for ID: " + rid);
     }
-}
-
+    }
 }
